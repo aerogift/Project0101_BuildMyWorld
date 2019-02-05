b47acc96de500357ea9458f5345c667cfc42ded9
SupawatROS <aerogift@gmail.com>
2019-02-04 20:04:35 -0800

    Add existing file

---
+++ script/welcome_message.cpp
@@ -0,0 +1,16 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginWelcome : public WorldPlugin
+  {
+    public: WorldPluginWelcome() : WorldPlugin()
+	    {
+		printf("Welcome to Supawat's World!\n");
+	    }
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+	    {
+	    }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginWelcome)
+}
